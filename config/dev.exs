72628777a0fd005245b8e334951360594d9dcb63
dideex <wc34fun@mail.ru>
2019-10-22 07:50:30 +0300

    ⛱chnage ports

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :backend, Backend.Repo,
 # watchers to your application. For example, we use it
 # with webpack to recompile .js and .css sources.
 config :backend, BackendWeb.Endpoint,
-  http: [port: 4000],
+  http: [port: 5000],
   debug_errors: true,
   code_reloader: true,
   check_origin: false,
