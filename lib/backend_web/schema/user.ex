9ed06bb072b30b6658f0d48bae97237d92357fd4
DiDex <wc34fun@mail.ru>
2020-05-20 06:54:37 +0300

    Rm some code

--- lib/backend_web/schema/user.ex
+++ lib/backend_web/schema/user.ex
@@ -2,7 +2,6 @@ defmodule Backend.Schema.User do
   use Absinthe.Schema
   alias Backend.Resolvers.Person
 
-  # import types
   import_types(Backend.Schema.Types)
 
   query do
@@ -10,11 +9,6 @@ defmodule Backend.Schema.User do
     field :get_users, list_of(:person) do
       resolve(&Person.get_users/2)
     end
-
-    # @spec "Test query returns user list"
-    # field :get_users, list_of(:person) do
-    #   resolve(&Person.get_users/2)
-    # end
 
     @spec "Get all my info by the token"
     field :me, :person do
